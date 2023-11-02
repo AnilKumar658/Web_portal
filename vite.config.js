0ef9648a0b272549d46ad7c2ab0315eb7fb14bf8
anil kumar <garlapatianil68@gmail.com>
2023-11-02 21:17:52 +0530

    add : deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-//build: {
-//  rollupOptions: {
-//    output: {
-//      // ...other options
-//      manualChunks(id) {
-//        if (id.includes('my-custom-module')) {
-//          return 'custom-chunk';
-//        }
-//    },
-//    },
-//  },
-//},
   build: {
     chunkSizeWarningLimit: 10000, // Adjust the limit to your desired value in kBs
-  }})
+  },
+  base: "/3d_portfolio"
+})
