b3d34dc9327a1c4694980b9f86841448bfbc5afb
anil kumar <garlapatianil68@gmail.com>
2023-11-02 20:46:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter as  BrowserRouter } from "react-router-dom";
 import {
   About,
   Contact,
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+//build: {
+//  rollupOptions: {
+//    output: {
+//      // ...other options
+//      manualChunks(id) {
+//        if (id.includes('my-custom-module')) {
+//          return 'custom-chunk';
+//        }
+//    },
+//    },
+//  },
+//},
+  build: {
+    chunkSizeWarningLimit: 10000, // Adjust the limit to your desired value in kBs
+  }})
